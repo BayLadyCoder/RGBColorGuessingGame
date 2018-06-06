b0b9944274f8a90fb4bfeb5d12b8d54b4b7defc0
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-06-06 17:30:48 -0400

    cleaned up code and added comments.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const rgbHeader = document.querySelector("#rgbHeader");
+const colorBoxes = [...document.querySelectorAll(".colorBoxes")];
+const headerContainer = document.querySelector(".headerContainer");
+const newColors = document.querySelector("#newColors");
+const lowerColorBoxes = document.querySelector("#lowerColorBoxes");
+const easyMode = document.querySelector("#easy");
+const hardMode = document.querySelector("#hard");
 
+////////////////////////////////////////////////////////////////
 function random255() {
   return Math.floor(Math.random()*256);
 }
 
+// create a set of random numbers
 function randomColor() {
   const red = random255();
   const green= random255();
@@ -19,109 +28,99 @@ function arrColors(num){
   return arrColors;
 }
 
-
-const rgbHeader = document.querySelector("#rgbHeader");
-const colorBoxes = [...document.querySelectorAll(".colorBoxes")];
-
 function newGame(num) {
   const arrColorsSet = arrColors(num);
   return arrColorsSet;
 };
 
-let arrColorsSet = newGame(6);
-
 function pickedColor() {
-    const randomPick = Math.floor(Math.random()*arrColorsSet.length);
-    const pickedColor = `rgb(${arrColorsSet[randomPick]})`;
-    return pickedColor;
+  const randomPick = Math.floor(Math.random()*arrColorsSet.length);
+  const pickedColor = `rgb(${arrColorsSet[randomPick]})`;
+  return pickedColor;
 };
 
+function generateColors() {
+  for (let i=0 ; i< colorBoxes.length ; i++) {
+    colorBoxes[i].style.background = `rgb(${arrColorsSet[i]})`;
+  }
+}
+
+function getNewColor() {
+  return rgbHeader.innerHTML = `RGB ${theColor}`;
+}
+
+let arrColorsSet = newGame(6);
 let theColor = pickedColor();
-
-  function generateColors() {
-    for (let i=0 ; i< colorBoxes.length ; i++) {
-      colorBoxes[i].style.background = `rgb(${arrColorsSet[i]})`;
-    }
-  }
-  ////////////////////////////////////////////////////////////////
-  function getNewColor() {
-    return rgbHeader.innerHTML = `RGB ${theColor}`;
-  }
-
-  getNewColor();
-  generateColors();
+getNewColor();
+generateColors();
 
 
 
-
-
-
-
+// Click Color Event that will find out if it's the correct color
 ////////////////////////////////////////////////////////////////
-const headerContainer = document.querySelector(".headerContainer");
-
 const clickedColor = colorBoxes.map(box => {
   box.addEventListener("click", function () {
-  const clickedColorVal = this.style.backgroundColor;
-  const pickedColorVal = theColor;
-  console.log(pickedColorVal);
-  console.log(clickedColorVal);
+    const clickedColorVal = this.style.backgroundColor;
+    const pickedColorVal = theColor;
+    console.log(pickedColorVal);
+    console.log(clickedColorVal);
 
-  if (clickedColorVal === pickedColorVal) {
-    return colorBoxes.map(box => {
-      const correctBGColor = clickedColorVal;
-      box.style = `background-color: ${clickedColorVal}; opacity: 1;`;
-      headerContainer.style = `background-color: ${clickedColorVal};`;
-    });
+    if (clickedColorVal === pickedColorVal) {
+      return colorBoxes.map(box => {
+        const correctBGColor = clickedColorVal;
+        box.style = `background-color: ${clickedColorVal}; opacity: 1;`;
+        headerContainer.style = `background-color: ${clickedColorVal};`;
+      });
+    }
+    else {
+      return this.style = "background: #232323; opacity: 0;";
+    }
+  })});
+  ////////////////////////////////////////////////////////////////
+
+
+
+  // Functions for Click Events
+  ////////////////////////////////////////////////////////////////
+  function backToDefault() {
+    theColor = pickedColor();
+    getNewColor();
+    generateColors();
+    headerContainer.style = `background-color: #067e82;`;
   }
-  else {
-    return this.style = "background: #232323; opacity: 0;";
+
+  function easyModeOn() {
+    arrColorsSet = newGame(3);
+    lowerColorBoxes.style.display = "none";
+    backToDefault();
   }
-})});
-////////////////////////////////////////////////////////////////
 
+  function hardModeOn() {
+    arrColorsSet = newGame(6);
+    lowerColorBoxes.style.display = "flex";
+    backToDefault();
+  }
 
-const newColors = document.querySelector("#newColors");
-const lowerColorBoxes = document.querySelector("#lowerColorBoxes");
-const easyMode = document.querySelector("#easy");
-const hardMode = document.querySelector("#hard");
+  // Click Event Handlers
+  ////////////////////////////////////////////////////////////////
+  newColors.addEventListener("click", () => {
+    if(easyMode.classList.contains("selectedMode")) {
+      return easyModeOn();
+    }
+    else {
+      return hardModeOn();
+    }
+  });
 
-easyMode.addEventListener("click", function() {
-  easyMode.classList.add("selectedMode");
-  hardMode.classList.remove("selectedMode");
-  return easyModeOn();
-});
-
-hardMode.addEventListener("click", function() {
-  hardMode.classList.add("selectedMode");
-  easyMode.classList.remove("selectedMode");
-  return hardModeOn();
-});
-
-function easyModeOn() {
-  arrColorsSet = newGame(3);
-  theColor = pickedColor();
-  getNewColor();
-  generateColors();
-  lowerColorBoxes.style.display = "none";
-  headerContainer.style = `background-color: #067e82;`;
-}
-
-function hardModeOn() {
-  arrColorsSet = newGame(6);
-  theColor = pickedColor();
-  getNewColor();
-  generateColors();
-  lowerColorBoxes.style.display = "flex";
-  headerContainer.style = `background-color: #067e82;`;
-}
-
-newColors.addEventListener("click", function (){
-  if(easyMode.classList.contains("selectedMode")) {
+  easyMode.addEventListener("click", () => {
+    easyMode.classList.add("selectedMode");
+    hardMode.classList.remove("selectedMode");
     return easyModeOn();
-  }
-  else {
+  });
+
+  hardMode.addEventListener("click", () => {
+    hardMode.classList.add("selectedMode");
+    easyMode.classList.remove("selectedMode");
     return hardModeOn();
-  }
-});
-////////////////////////////////////////////////////////////////
+  });
+  ////////////////////////////////////////////////////////////////
