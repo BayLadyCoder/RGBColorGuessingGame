69cb8beb745553347a8e38d43e272fdc7ebdb4b6
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-06-05 21:18:39 -0400

    added randomColor to each boxes, added pickedColor, and getNewColor to reset the  game.

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 
+function random255() {
+  return Math.floor(Math.random()*256);
+}
 
-const red = Math.floor(Math.random()*255);
-const green = Math.floor(Math.random()*255);
-const blue = Math.floor(Math.random()*255);
-const randomColor = `${red}, ${green}, ${blue}`;
+function randomColor() {
+  const red = random255();
+  const green= random255();
+  const blue= random255();
+  return `${red}, ${green}, ${blue}`;
+}
+
+const arrColors = [];
+
+// Create an array of set of random numbers
+for (let i=0 ; i < 6; i++) {
+  arrColors[i] = `${randomColor()}`;
+}
+
+console.log(arrColors);
 
 const rgbHeader = document.querySelector("#rgbHeader");
 
+function pickedColor() {
+  const randomPick = Math.floor(Math.random()*arrColors.length)
+  return arrColors[randomPick];
+}
+
 function getNewColor() {
-  rgbHeader.innerHTML = `${randomColor}`;
+  rgbHeader.innerHTML = `RGB ${pickedColor()}`;
 }
 
 getNewColor();
+
+const colorBoxes = [...document.querySelectorAll(".colorBoxes")];
+
+for (let i=0 ; i< colorBoxes.length ; i++) {
+  colorBoxes[i].style.background = `rgb(${arrColors[i]})`;
+}
+console.log(colorBoxes);
